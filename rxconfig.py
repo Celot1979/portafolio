eb03c622dd2cfce3bceae90b7882d560fdd86831
Celot1979 <zata_k@hotmail.com>
2025-07-25 11:01:03 +0200

    Deploy to GitHub Pages with correct deploy_url

--- rxconfig.py
+++ rxconfig.py
@@ -4,5 +4,6 @@ APP_MODULE = "portafolio.portafolio:app"
 
 config = rx.Config(
     app_name="portafolio",
+    deploy_url="https://celot1979.github.io/portafolio/",
     plugins=[rx.plugins.TailwindV3Plugin()],
 )
