5a0f3d23bdb3414eec595d30256991b8ed4d08dd
Celot1979 <zata_k@hotmail.com>
2025-07-25 11:57:11 +0200

    Fix HTML paths for GitHub Pages deployment

--- rxconfig.py
+++ rxconfig.py
@@ -5,5 +5,6 @@ APP_MODULE = "portafolio.portafolio:app"
 config = rx.Config(
     app_name="portafolio",
     api_url="https://celot1979.github.io/portafolio/",
+    next_base_path="/portafolio",
     plugins=[rx.plugins.TailwindV3Plugin()],
 )
